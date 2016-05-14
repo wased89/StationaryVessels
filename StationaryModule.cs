12880a4233d96d0f4de55daa1e769d17f6288a02
Fox <Fox@Hole.yiff>
2016-05-14 15:57:00 -0400

    initial commit

---
+++ StationaryModule.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace StationaryVessels
+{
+    public class StationaryModule : PartModule
+    {
+        [Persistent]
+        private static bool isFrozen;
+        [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "isFrozen: false")]
+        public void ToggleFreeze()
+        {
+            isFrozen = !isFrozen;
+            Events["ToggleFreeze"].guiName = isFrozen ? "isFrozen: true": "isFrozen: false";
+            Debug.Log("IsFrozen: "+ isFrozen);
+        }
+
+        [KSPAction("Toggle Freeze")]
+        public void ToggleFreezeAction(KSPActionParam param)
+        {
+            ToggleFreeze();
+        }
+        public void FixedUpdate()
+        {
+            Debug.Log("FixedUpdate");
+            if(isFrozen && !vessel.isEVA)
+            {
+                foreach (Part p in vessel.parts)
+                {
+                    if (p.GetComponent<Rigidbody>() == null) { continue; }
+                    p.GetComponent<Rigidbody>().isKinematic = true;
+                    Debug.Log("Freezing part");
+                }
+            }
+        }
+    }
+}
---
+++ StationaryVesselModule.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace StationaryVessels
+{
+    public class StationaryVesselModule : VesselModule
+    {
+        private Vessel vessel;
+
+        public StationaryVesselModule()
+        {
+            Debug.Log("ctor called");
+        }
+        public void Start()
+        {
+            Debug.Log("StationaryStart");
+            vessel = GetComponent<Vessel>();
+            if(!vessel.isEVA)
+            foreach(Part p in vessel.parts)
+            {
+                if(p.Modules.Contains("ModuleCommand"))
+                {
+                    Debug.Log("Adding module");
+                    p.AddModule("StationaryModule");
+                }
+            }
+        }
+    }
+}
